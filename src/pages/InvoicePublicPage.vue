5535eeb49354780f67cd7949bef0ae5c28d4230c
Eduardo Donato <eduardo.donato@gmail.com>
2022-06-19 15:43:24 -0300

    dense invoice print layout

--- src/pages/InvoicePublicPage.vue
+++ src/pages/InvoicePublicPage.vue
@@ -9,29 +9,31 @@
         </div>
         <div v-else class="column content-center q-pa-xs q-gutter-xs">
             <q-card flat bordered :dark="false" class="invoice-card">
-                <q-card-section>
-                    <div class="text-h2">{{ info.fantasyName || info.name }}</div>
-                    <div class="row q-gutter-md" v-if="info.fantasyName || info.nationalRegistration">
-                        <div v-if="info.fantasyName" class="text-h6">{{ info.name }}</div>
-                        <div v-if="info.nationalRegistration" class="text-h6">{{ info.person == "legal" ? "CNPJ" : "CPF" }}: {{ formatCPForCNPJ(info.nationalRegistration) }}</div>
+                <q-card-section class="q-pa-xs">
+                    <div class="row q-gutter-sm">
+                        <div class="col-grow text-h5">{{ info.fantasyName || info.name }}</div>
+                        <div class="self-end" v-if="info.fantasyName">{{ info.name }}</div>
+                        <div class="self-end" v-if="info.nationalRegistration">{{ info.person == "legal" ? "CNPJ" : "CPF" }}: {{ formatCPForCNPJ(info.nationalRegistration) }}</div>
                     </div>
-                    <div class="row" v-if="info.addressLine1 || info.addressLine2 || info.city || info.state">
-                        <div class="q-mr-md text-h5">
-                            <q-icon name="business" />
+                    <div class="row q-gutter-sm">
+                        <div class="row col" v-if="info.addressLine1 || info.addressLine2 || info.city || info.state">
+                            <div class="q-mr-sm">
+                                <q-icon name="business" />
+                            </div>
+                            <div>
+                                <div>{{ info.addressLine1 }}<span v-if="info.addressLine1 && info.addressLine2"> - </span>{{ info.addressLine2 }}</div>
+                                <div>{{ info.city }}<span v-if="info.city && info.state"> - </span>{{ info.state }}</div>
+                            </div>
                         </div>
-                        <div>
-                            <div>{{ info.addressLine1 }}<span v-if="info.addressLine1 && info.addressLine2"> - </span>{{ info.addressLine2 }}</div>
-                            <div>{{ info.city }}<span v-if="info.city && info.state"> - </span>{{ info.state }}</div>
+                        <div class="col">
+                            <div class="text-right" v-if="info.phone"><q-icon name="perm_phone_msg" class="q-mr-sm" /> {{ formatPhone(info.phone) }}</div>
+                            <div class="text-right" v-if="info.email"><q-icon name="mail" class="q-mr-sm" /> {{ info.email }}</div>
                         </div>
-                    </div>
-                    <div class="row q-gutter-md">
-                        <div v-if="info.phone" class="text-h5"><q-icon name="perm_phone_msg" class="q-mr-sm" /> {{ formatPhone(info.phone) }}</div>
-                        <div v-if="info.email" class="text-h5"><q-icon name="mail" class="q-mr-sm" /> {{ info.email }}</div>
                     </div>
                 </q-card-section>
             </q-card>
             <q-card flat bordered :dark="false" class="invoice-card">
-                <q-card-section>
+                <q-card-section class="q-pa-xs">
                     <div>Orçamento: {{ invoiceId }}</div>
                     <div class="row">
                         <div class="col">Data: {{ invoice.date }}</div>
@@ -44,10 +46,10 @@
                 </q-card-section>
             </q-card>
             <q-card flat bordered :dark="false" class="invoice-card">
-                <q-markup-table :dark="false" separator="cell">
+                <q-markup-table :dark="false" separator="cell" dense>
                     <thead>
                         <tr>
-                            <th class="text-right">#</th>
+                            <th class="text-center">#</th>
                             <th class="table-main-column text-left">Descrição</th>
                             <th class="text-right">Quantidade</th>
                             <th class="text-right">Valor (R$)</th>
@@ -56,7 +58,7 @@
                     </thead>
                     <tbody>
                         <tr v-for="(item, index) of invoice.items" :key="index">
-                            <td class="text-right">{{ index + 1 }}</td>
+                            <td class="text-center">{{ index + 1 }}</td>
                             <td class="table-main-column text-left">{{ item.description }}</td>
                             <td class="text-right">{{ item.quantity }}</td>
                             <td class="text-right">{{ item.value }}</td>
@@ -69,11 +71,11 @@
                 </q-markup-table>
             </q-card>
             <q-card flat bordered :dark="false" class="invoice-card">
-                <q-card-section class="q-pb-none">
-                    <div class="text-weight-medium">Observações</div>
+                <q-card-section class="row q-pa-xs align-start">
+                    <div class="text-weight-medium q-mr-xs">Observações:</div>
+                    <div v-if="!!invoice.note" v-html="$sanitize(invoice.note)" />
+                    <div v-else>Sem Observações</div>
                 </q-card-section>
-                <q-card-section v-if="!!invoice.note" v-html="$sanitize(invoice.note)" />
-                <q-card-section v-else>Sem Observações</q-card-section>
             </q-card>
         </div>
     </q-page>
@@ -178,21 +180,16 @@ loadData()
     border: 2px solid #000000;
 }
 
+.q-table th,
+.q-table td {
+    border-color: rgba($color: #000000, $alpha: 0.5);
+}
+
 .table-main-column {
     width: 70%;
 }
 .invoice-card {
-    width: 80%;
+    width: 100%;
     max-width: 800px;
-}
-
-@media print {
-    .q-table th,
-    .q-table td {
-        border-color: rgba($color: #000000, $alpha: 0.5);
-    }
-    .invoice-card {
-        width: 100%;
-    }
 }
 </style>
