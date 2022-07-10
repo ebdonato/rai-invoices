5edf028ea74f024068ec2780b671b6584100dad7
Eduardo Donato <eduardo.donato@gmail.com>
2022-07-09 22:22:57 -0300

    refactor public view header layout

--- src/components/InfoPublicHeader.vue
+++ src/components/InfoPublicHeader.vue
@@ -3,32 +3,36 @@
         <div v-if="isLoading" class="text-center">
             <q-circular-progress indeterminate size="50px" :thickness="0.22" color="grey-6" track-color="grey-3" class="q-ma-md" />
         </div>
-        <q-card-section v-else class="q-pa-xs">
-            <div class="row">
-                <div v-if="info.logoUrl" class="column q-mx-xs justify-center" style="min-width: 70px; max-height: 70px" :class="{ 'col-grow': isLogoWider }">
-                    <q-img :src="info.logoUrl" fit="contain" class="rounded-borders" @load="onLoad"></q-img>
+        <q-card-section v-else class="row q-pa-none">
+            <div v-if="info.logoUrl" class="column q-mx-xs justify-center" style="min-width: 70px; max-height: 95px" :class="{ 'col-grow': isLogoWider }">
+                <q-img :src="info.logoUrl" fit="contain" class="rounded-borders" @load="onLoad"></q-img>
+            </div>
+            <div class="col-grow row q-mx-xs">
+                <div class="column col justify-between">
+                    <div class="text-h5">{{ info.fantasyName || info.name }}</div>
+                    <div class="row no-wrap" v-if="info.addressLine1 || info.addressLine2">
+                        <div class="q-mr-sm">
+                            <q-icon name="business" />
+                        </div>
+                        <div>
+                            <div>{{ info.addressLine1 }}</div>
+                            <div>{{ info.addressLine2 }}</div>
+                        </div>
+                    </div>
+                    <div class="row no-wrap" v-if="info.city || info.state">
+                        <div class="q-mr-sm">
+                            <q-icon name="map" />
+                        </div>
+                        <div>
+                            <div>{{ info.city }}<span v-if="info.city && info.state"> - </span>{{ info.state }}</div>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-grow q-mx-xs">
-                    <div class="row q-gutter-sm">
-                        <div class="col-grow text-h5">{{ info.fantasyName || info.name }}</div>
-                        <div class="self-end" v-if="info.fantasyName">{{ info.name }}</div>
-                        <div class="self-end" v-if="info.nationalRegistration">{{ info.person == "legal" ? "CNPJ" : "CPF" }}: {{ formatCPForCNPJ(info.nationalRegistration) }}</div>
-                    </div>
-                    <div class="row q-gutter-sm">
-                        <div class="row col" v-if="info.addressLine1 || info.addressLine2 || info.city || info.state">
-                            <div class="q-mr-sm">
-                                <q-icon name="business" />
-                            </div>
-                            <div>
-                                <div>{{ info.addressLine1 }}<span v-if="info.addressLine1 && info.addressLine2"> - </span>{{ info.addressLine2 }}</div>
-                                <div>{{ info.city }}<span v-if="info.city && info.state"> - </span>{{ info.state }}</div>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="text-right" v-if="info.phone"><q-icon name="perm_phone_msg" class="q-mr-sm" /> {{ formatPhone(info.phone) }}</div>
-                            <div class="text-right" v-if="info.email"><q-icon name="mail" class="q-mr-sm" /> {{ info.email }}</div>
-                        </div>
-                    </div>
+                <div class="column col justify-between">
+                    <div class="text-right" v-if="info.nationalRegistration">{{ info.person == "legal" ? "CNPJ" : "CPF" }}: {{ formatCPForCNPJ(info.nationalRegistration) }}</div>
+                    <div class="text-right" v-if="info.fantasyName">{{ info.name }}</div>
+                    <div class="text-right" v-if="info.email"><q-icon name="mail" class="q-mr-sm" /> {{ info.email }}</div>
+                    <div class="text-right" v-if="info.phone"><q-icon name="perm_phone_msg" class="q-mr-sm" /> {{ formatPhone(info.phone) }}</div>
                 </div>
             </div>
         </q-card-section>
