0e93e418f3a86eebc79706cea6c7bb16f4f05de1
Eduardo Donato <eduardo.donato@gmail.com>
2022-07-11 00:00:54 -0300

    add forgot email routine

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -17,6 +17,7 @@
                 <LoginCard />
                 <q-btn color="primary" icon="fa-brands fa-google" label="Entrar com Google" @click="signIn" />
                 <q-btn flat no-caps size="md" label="Ainda não tenho conta" @click="isCreatingUser = !isCreatingUser" color="white" />
+                <q-btn flat no-caps size="md" label="Esqueci a senha" @click="passwordReset" color="white" />
             </div>
         </div>
         <q-btn
@@ -36,7 +37,7 @@
 <script setup>
 import { ref } from "vue"
 import { useQuasar } from "quasar"
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth"
+import { GoogleAuthProvider, getAuth, signInWithPopup, sendPasswordResetEmail } from "firebase/auth"
 
 import CreateUserCard from "components/CreateUserCard.vue"
 import LoginCard from "components/LoginCard.vue"
@@ -65,5 +66,40 @@ const signIn = () => {
         .finally(() => {
             $q.loading.hide()
         })
+}
+
+const passwordReset = () => {
+    $q.dialog({
+        title: "Esqueci a senha",
+        message: "Enviar e-mail de recuperação para qual endereço?",
+        prompt: {
+            model: "",
+            isValid: (val) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(val),
+            type: "text",
+        },
+        cancel: true,
+        persistent: true,
+    }).onOk((email) => {
+        $q.loading.show()
+        sendPasswordResetEmail(getAuth(), email)
+            .then(() => {
+                $q.notify({
+                    type: "positive",
+                    message: "E-mail de recuperação enviado",
+                })
+            })
+            .catch((error) => {
+                console.error(error)
+
+                $q.notify({
+                    type: "negative",
+                    message: "Erro ao enviar e-mail de recuperação",
+                    caption: error.message,
+                })
+            })
+            .finally(() => {
+                $q.loading.hide()
+            })
+    })
 }
 </script>
