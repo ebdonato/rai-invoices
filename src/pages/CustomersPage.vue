de73bead7a1644c015b7cb1588ecf7e977cd4cf1
Eduardo Donato <eduardo.donato@gmail.com>
2023-07-14 20:56:12 -0300

    add no items feedback to customers page

--- src/pages/CustomersPage.vue
+++ src/pages/CustomersPage.vue
@@ -54,6 +54,7 @@
                             </div>
                         </q-card-section>
                     </q-card>
+                    <q-banner v-if="customers.length === 0" rounded inline-actions class="full-width">Nenhum item {{ filter ? "encontrado" : "cadastrado" }}</q-banner>
                 </div>
 
                 <div style="min-height: 70px" class="flex column justify-center align-center">
